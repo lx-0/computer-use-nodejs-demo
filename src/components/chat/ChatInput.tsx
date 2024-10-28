519bb8615b774e9f9131e601f2dd964043d0be42
Alex Wegener <github.com@m.9aw.de>
2024-10-28 11:37:10 +0100

    submit message on enter

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import React from 'react';
+import React, { KeyboardEvent } from 'react';
 
 interface ChatInputProps {
   value: string;
@@ -9,11 +9,19 @@ interface ChatInputProps {
 }
 
 export const ChatInput: React.FC<ChatInputProps> = ({ value, onChange, onSend }) => {
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      onSend();
+    }
+  };
+
   return (
     <div className="flex">
       <Input
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder="Type your message..."
         className="flex-grow mr-2"
       />
